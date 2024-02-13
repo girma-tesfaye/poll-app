124e044747f6cfd7d2a0fbbd265aa0b9674447ca
girma-tesfaye <girmatesfaye80@gmail.com>
2024-02-13 00:16:42 -0800

    Reusable tabs component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,24 @@
 <script>
     import Footer from "./components/Footer.svelte";
 	import Header from "./components/Header.svelte";
+    import Tabs from "./reusable/Tabs.svelte";
 
+	//tabs
+	let items = ['Current Polls', 'Add New Poll']
+	let activeItem = ['Current Polls']
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 	
 </script>
 <Header/>
 <main>
-	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente laudantium adipisci officiis esse ducimus perspiciatis animi tempora dolore amet iste ad voluptatibus omnis accusantium veritatis totam cumque consectetur, maiores hic!</p>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<p>List of polls component comes here</p>
+	{:else if activeItem === 'Add New Poll'}
+		<p>New poll form component comes here</p>
+	{/if}
 </main>
 <Footer/>
 
---
+++ src/reusable/Tabs.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    export let items;
+    export let activeItem;
+
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each items as item }
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <li on:click={() => dispatch('tabChange', item)}>
+                <div class:active={item == activeItem} >{item}</div>
+            </li>
+        {/each}
+    </ul>
+</div>
+<style>
+    .tabs {
+        margin-bottom: 40px;
+    }
+    ul {
+        display: flex;
+        list-style-type: none;
+        padding: 0;
+        justify-content: center;
+    }
+    li {
+        margin: 0 15px;
+        font-size: 18px;
+        font-weight: 600;
+        font-family: "Trirong", serif;
+        color: #555;
+        cursor: pointer;
+    }
+    .active {
+        color: #e30327;
+        border-bottom: 2px solid #e30327;
+        padding-bottom: 8px;
+    }
+    
+</style>
