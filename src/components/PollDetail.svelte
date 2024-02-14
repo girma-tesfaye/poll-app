cf5ddd1b02290578d56140ed9d84c3f4a8c41596
girma-tesfaye <girmatesfaye80@gmail.com>
2024-02-14 10:57:51 -0800

    Add vote bars in percentage

--- src/components/PollDetail.svelte
+++ src/components/PollDetail.svelte
@@ -5,6 +5,8 @@
     const dispatch = createEventDispatcher();
     export let poll;
     $: totalVotes = poll.voteA + poll.voteB;
+    $: percentA = Math.floor(poll.voteA/totalVotes*100)
+    $: percentB = Math.floor(poll.voteB/totalVotes*100)
 
     const handleVote = (option, id) => {
         dispatch('vote', {option, id})
@@ -17,12 +19,12 @@
         <p>Total votes: { totalVotes }</p>
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div class="answer" on:click={() => handleVote('a', poll.id)}>
-            <div class="percent percent-a"></div>
+            <div class="percent percent-a" style="width: {percentA}%"></div>
             <span> { poll.answerA } ({poll.voteA})</span>
         </div>
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div class="answer" on:click={() => handleVote('b', poll.id)}>
-            <div class="percent percent-b"></div>
+            <div class="percent percent-b" style="width: {percentB}%"></div>
             <span> { poll.answerB } ({poll.voteB})</span>
         </div>
     </div>
@@ -50,5 +52,18 @@
     span{
         display: inline-block;
         padding: 10px 20px;
+    }
+    .percent{
+        height: 100%;
+        position: absolute;
+        box-sizing: border-box;
+    }
+    .percent-a{
+        border-left: 4px solid #d91b42;
+        background: rgba(217, 27, 26, 0.2);
+    }
+    .percent-b{
+        border-left: 4px solid #45c496;
+        background: rgba(69, 196, 150, 0.2);
     }
 </style>
