ba0a0a87afbb1085ac13266e155d7a5d1b90a7b3
girma-tesfaye <girmatesfaye80@gmail.com>
2024-02-14 11:47:56 -0800

    Lifecycle hooks

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,17 +1,29 @@
 <script>
+    //import { onDestroy, onMount } from "svelte";
     import PollStore from "../stores/store"
     import PollDetail from "./PollDetail.svelte";
 
-    export let polls = [];
+    // export let polls = [];
 
-    PollStore.subscribe(data => {
-        polls = data;
-    });
+    // const unsub = PollStore.subscribe(data => {
+    //     polls = data;
+    // });
+
+    // onMount(() => {
+    //     //maybe get data from a db
+    //     console.log("component mounted");
+    //     unsub();
+    // });
+
+    // onDestroy(() => {
+    //     //unsub from store
+    //     console.log("Component destroyed");
+    // });
 
 </script>
 
 <div class="poll-list">
-    {#each polls as poll (poll.id)}
+    {#each $PollStore as poll (poll.id)}
         <PollDetail {poll} on:vote/>
     {/each}
 </div>
